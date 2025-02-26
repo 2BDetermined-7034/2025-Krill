354ae1b44e0f73cb582195d997ede7073e3eba15
Henry <erickson.henry.d@gmail.com>
2025-02-25 17:55:36 -0800

    initial

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -13,6 +13,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandPS5Controller;
 
+import frc.robot.commands.Intake.IntakeAlgaeCommand;
 import frc.robot.commands.Intake.IntakeCommand;
 import frc.robot.commands.Intake.OuttakeCommand;
 import frc.robot.generated.TunerConstants;
@@ -71,6 +72,7 @@ public class RobotContainer {
 
 		joystick.R2().whileTrue(new IntakeCommand(arm));
 		joystick.L2().whileTrue(new OuttakeCommand(arm));
+		joystick.L1().whileTrue(new IntakeAlgaeCommand(arm));
 
 		//joystick.triangle().onTrue(elevator.setElevatorPosition(Rotations.of(1.5)));
 		//joystick.square().onTrue(elevator.setElevatorPosition(Rotations.of(0)));
@@ -87,6 +89,7 @@ public class RobotContainer {
 		joystick.povLeft().whileTrue(elevator.setElevatorPosition(ScoringPosition.L3));
 		joystick.povRight().whileTrue(elevator.setElevatorPosition(ScoringPosition.L2));
 		joystick.povRight().and(joystick.cross()).whileTrue(elevator.setElevatorPosition(ScoringPosition.L1));
+
 
 
 
