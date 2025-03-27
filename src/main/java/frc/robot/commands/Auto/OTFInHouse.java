1b20ca2c3c6ae2f3cfc0d9ad996f2b78afd9a78e
Jack M <101893497+mopo3ob-jack@users.noreply.github.com>
2025-03-27 09:57:24 -0700

    Robot Relative OTF

--- src/main/java/frc/robot/commands/Auto/OTFInHouse.java
+++ src/main/java/frc/robot/commands/Auto/OTFInHouse.java
@@ -1,8 +1,11 @@
 package frc.robot.commands.Auto;
 
 import com.ctre.phoenix6.swerve.SwerveRequest;
+import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.*;
+import edu.wpi.first.math.numbers.N2;
+import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -28,16 +31,21 @@ public class OTFInHouse {
 		omegaController.enableContinuousInput(-0.5, 0.5);
 
 		AtomicReference<Translation2d> previousVelocity = new AtomicReference<>(new Translation2d(0.0, 0.0));
+		AtomicReference<Transform2d> tagLocalTransform = new AtomicReference<>();
 
 		return Commands.runOnce(() -> {
-			xController.setSetpoint(setpoint.get().getX());
-			yController.setSetpoint(setpoint.get().getY());
-			omegaController.setSetpoint(setpoint.get().getRotation().getRotations());
+			tagLocalTransform.set(new Transform2d(setpoint.get(), new Pose2d(0.0, 0.0, new Rotation2d(0.0))));
+			Pose2d transformedSetpoint = setpoint.get().transformBy(tagLocalTransform.get());
+			xController.setSetpoint(transformedSetpoint.getX());
+			yController.setSetpoint(transformedSetpoint.getY());
+			omegaController.setSetpoint(transformedSetpoint.getRotation().getRotations());
 			SmartDashboard.putNumberArray("Pose setpoint", new double[] {setpoint.get().getX(), setpoint.get().getY(), setpoint.get().getRotation().getDegrees()});
+			SmartDashboard.putNumberArray("Transformed pose setpoint", new double[] {transformedSetpoint.getX(), transformedSetpoint.getY(), transformedSetpoint.getRotation().getDegrees()});
 		}).andThen(drivetrain.applyRequest(() -> {
+			Pose2d transformedPose = drivetrain.getPose().transformBy(tagLocalTransform.get());
 			Translation2d velocity = new Translation2d(
-				-xController.calculate(drivetrain.getPose().getX()),
-				-yController.calculate(drivetrain.getPose().getY())
+				-xController.calculate(transformedPose.getX()),
+				-yController.calculate(transformedPose.getY())
 			);
 
 			Translation2d deltaVelocity = velocity.minus(previousVelocity.get());
@@ -55,7 +63,7 @@ public class OTFInHouse {
 
 			previousVelocity.set(velocity);
 
-			double omegaVelocity = omegaController.calculate(drivetrain.getPose().getRotation().getRotations());
+			double omegaVelocity = omegaController.calculate(transformedPose.getRotation().getRotations());
 
 			return drive
 				.withVelocityX(velocity.getX())
